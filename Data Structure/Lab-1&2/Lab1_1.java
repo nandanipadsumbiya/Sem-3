f65dd5d22eb92c11f137af942456f77766830d1d
nandanipadsumbiya <145186700+nandanipadsumbiya@users.noreply.github.com>
2024-06-23 10:38:40 +0530

    Delete Data Structure/Lab-1&2 directory

--- Data Structure/Lab-1&2/Lab1_1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Lab1_1{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int r=sc.nextInt();
-        double b=Math.PI*r*r;
-        System.out.println(b);
-    }
-}
